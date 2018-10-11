de76fe139dd47ee9f4b5461b42f6cc2eff81aa2c
Avneesh Sarwate <avneeshsarwate@gmail.com>
2018-10-11 15:47:40 -0400

    more implementation

--- js/midiDetector.js
+++ js/midiDetector.js
@@ -11,6 +11,15 @@ time since each note
 
 var timeWindow = 20;
 
+function getSeenPitches(windowedEventList, onNotes){
+    var seenNotes = Set();
+    windowedEventList.forEach(function(evt){
+        if(evt.type === 'on') seenNotes.add(evt.note);
+    });
+    seenNotes = seenNotes.union(onNotes);
+    return seenNotes;
+}
+
 function windowEventList(eventList, windowTime){
     var len = eventList.length;
     var offset = 0;
@@ -22,14 +31,24 @@ function windowEventList(eventList, windowTime){
 }
 
 function splitHands(windowedEventList, onNotes){
+    var seenNotes = getSeenPitches(windowedEventList, onNotes);
+    seenNotes = Array.from(seenNotes).sort();
 
+    var distToNextNote = [];
+    for(var i = 0; i < seenNotes.length-1; i++){
+        distToNextNote.push([i, seenNotes[i+1]-seenNotes[i]]);
+    } 
+
+    distToNextNote.sort(elem => elem[1]);
+    var maxDiffIndex = distToNextNote[distToNextNote.length-1][0];
+    var lowerHandMax = seenNotes[maxDiffIndex];
 }
 
 function avgSlope(windowedEventList, handNotes){
 
 }
 
-function avgNoteLength(windowedEventList){
+function avgNoteLength(windowedEventList, onNotes, onTimes, ignoreThresh){
 
 }
 
@@ -37,6 +56,21 @@ function triggerSustainEvent(onNotes, onTimes){
 
 }
 
-function tonalAvg(windowedEventList){
+function tonalAvg(windowedEventList, onNotes){
+    var diatonic = [0, 2, 4, 5, 7, 9, 11];
+    var seenNotes = getSeenPitches(windowedEventList, onNotes);
+    var tonicRatios = [];
 
+    for(var i = 0; i < 12; i++){
+        var keyChroma = new Set(diatonic.map(p => (i+p)%12));
+        var inKey = 0;
+        var outKey = 0;
+        seenNotes.forEach(function(note){
+            if(keyChroma.has(note%12)) inKey++;
+            else outKey++;
+        });
+        tonicRatios.push(inKey/(inKey+outKey));
+    }
+
+    return Math.max(...tonicRatios);
 }
