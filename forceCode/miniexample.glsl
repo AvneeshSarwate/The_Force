b72f497206de492985351fc378d21cf45fddbabd
Avneesh Sarwate <avneeshsarwate@gmail.com>
2019-08-23 16:19:25 -0400

    talk code

---
+++ forceCode/miniexample.glsl
@@ -0,0 +1,7 @@
+void main() {
+    vec2 stN = uvN();
+    vec2 cent = vec2(sinN(time), cosN(time))*0.5 + 0.25;
+    float col = distance(stN, cent) < 0.1 ? 0. : 1.;
+
+    gl_FragColor = vec4(col);
+}
