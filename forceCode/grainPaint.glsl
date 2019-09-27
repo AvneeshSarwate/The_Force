182ef94ae1031bb9ef2ed41d1065267def04f232
Avneesh Sarwate <avneeshsarwate@gmail.com>
2019-09-27 16:07:26 -0400

    new shader

---
+++ forceCode/grainPaint.glsl
@@ -0,0 +1,121 @@
+
+
+float colourDistance(vec3 e1, vec3 e2) {
+  float rmean = (e1.r + e2.r ) / 2.;
+  float r = e1.r - e2.r;
+  float g = e1.g - e2.g;
+  float b = e1.b - e2.b;
+  return sqrt((((512.+rmean)*r*r)/256.) + 4.*g*g + (((767.-rmean)*b*b)/256.));
+}
+
+
+vec3 coordWarp(vec2 stN, float t2){ 
+    vec2 warp = stN;
+    
+    float rad = .5;
+    
+    for (float i = 0.0; i < 20.; i++) {
+        vec2 p = vec2(sinN(t2* rand(i+1.) * 1.3 + i), cosN(t2 * rand(i+1.) * 1.1 + i));
+        warp = length(p - stN) <= rad ? mix(p, warp, length(stN - p)/rad)  : warp;
+    }
+    
+    return vec3(warp, distance(warp, stN));
+}
+
+float logisticSigmoid (float x, float a){
+  // n.b.: this Logistic Sigmoid has been normalized.
+
+  float epsilon = 0.0001;
+  float min_param_a = 0.0 + epsilon;
+  float max_param_a = 1.0 - epsilon;
+  a = max(min_param_a, min(max_param_a, a));
+  a = (1./(1.-a) - 1.);
+
+  float A = 1.0 / (1.0 + exp(0. -((x-0.5)*a*2.0)));
+  float B = 1.0 / (1.0 + exp(a));
+  float C = 1.0 / (1.0 + exp(0.-a)); 
+  float y = (A-B)/(C-B);
+  return y;
+}
+
+float stepTime(float t, float a){
+    return floor(t) + logisticSigmoid(fract(t), a);
+}
+
+float rand2(float f, float d) {vec2 n = vec2(f); return (fract(1e4 * sin(17.0 * n.x + n.y * 0.1) * (0.1 + abs(sin(n.y * 13.0 + n.x))))-0.5)*d;}
+
+vec4 avgColorBB(vec2 nn, float dist, float d){
+    vec4 c1 = texture2D(backbuffer, nn+vec2(0, dist)      +rand2(1., d)).rgba;
+    vec4 c2 = texture2D(backbuffer, nn+vec2(0, -dist)     +rand2(2., d)).rgba;
+    vec4 c3 = texture2D(backbuffer, nn+vec2(dist, 0)      +rand2(3., d)).rgba;
+    vec4 c4 = texture2D(backbuffer, nn+vec2(-dist, 0)     +rand2(4., d)).rgba;
+    vec4 c5 = texture2D(backbuffer, nn+vec2(dist, dist)   +rand2(5., d)).rgba;
+    vec4 c6 = texture2D(backbuffer, nn+vec2(-dist, -dist) +rand2(6., d)).rgba;
+    vec4 c7 = texture2D(backbuffer, nn+vec2(dist, -dist)  +rand2(7., d)).rgba;
+    vec4 c8 = texture2D(backbuffer, nn+vec2(-dist, dist)  +rand2(8., d)).rgba;
+    
+    return (c1+c2+c3+c4+c5+c6+c7+c8)/8.;
+}
+
+//the backbuffer uniform is a texture that stores the last rendered frame
+//this example shows how I use it to do feedback/trail effects
+void ex3() {
+    float t = stepTime(time, 0.);
+    
+    
+    vec2 stN = uvN();
+    vec2 warpN = coordWarp(stN, time).xy;
+    
+    
+    
+    vec2 camPos = vec2(1.-stN.x, stN.y); //flip the input x dimension because the macbook camera doesn't mirror the image
+    vec3 cam = texture2D(channel0, camPos).rgb; 
+    vec3 snap = texture2D(channel3, camPos).rgb;
+
+
+
+    vec4 bb = texture2D(backbuffer, stN);
+    vec4 bbAvg = avgColorBB(mix(stN, warpN, 0.005), 0.00, 0.002);
+    float lastFeedback = bbAvg.a;
+
+    bool condition = colourDistance(cam, snap) < .5;
+    
+    vec2 circ = vec2(sinN(t), cosN(t))*0.5 + .25;
+    vec2 mouseN = mouse.xy/2./resolution.xy;
+    // circ = vec2(mouseN.x, 1.-mouseN.y);
+    float rad = 0.1;
+    
+    vec3 hashN = hash(vec3(stN, .5)) / max(1000., pow(distance(stN, circ), .1)*100.);
+    
+    condition = distance(mix(stN + hashN.xy*100., warpN, 0.), circ) > rad;
+    
+    vec3 c;
+    float feedback; 
+    if(condition){
+        feedback = lastFeedback * 0.98;
+    } 
+    else{
+        feedback = 1.;
+    }
+    
+    float numLines = 50.;
+    float gridThickness = 0.003;
+    if(mod(stN.x, 1./numLines) < gridThickness || mod(stN.y, 1./numLines) < gridThickness) c =black;
+    else c = white;
+    
+    
+    float noiseval = snoise(vec3(stN*2. + hashN.xy*100., time/5.));
+    c = noiseval  > 0.3 ? black : mix(black, white, 1.);
+    
+    vec3 col = feedback == 1. ? c : bbAvg.rgb;
+    float trailCut = 0.05;
+    if(feedback < trailCut) col = mix(col, black, (trailCut-feedback)/trailCut);
+    // col = vec3(feedback);
+    
+    gl_FragColor = vec4(col, feedback);
+}
+
+
+void main(){
+    ex3();
+}
