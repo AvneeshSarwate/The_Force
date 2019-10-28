e57a6f2a4de2dd775152555d9def5167aa3c0fff
Avneesh Sarwate <avneeshsarwate@gmail.com>
2019-10-27 21:48:31 -0400

    undoing copy-paste mixup

--- forceCode/laserGlow_slider.glsl
+++ forceCode/laserGlow_slider.glsl
@@ -16,6 +16,151 @@ vec2 quant(vec2 num, float quantLevels){
     return (floor(num*quantLevels)+roundPart)/quantLevels;
 }
 
+/* bound a number to [low, high] and "wrap" the number back into the range
+if it exceeds the range on either side - 
+for example wrap(10, 1, 9) -> 8
+and wrap (-2, -1, 9) -> 0
+*/
+float wrap3(float val, float low, float high){
+    float range  = high - low;
+    if(val > high){
+        float dif = val-high;
+        float difMod = mod(dif, range);
+        float numWrap = dif/range - difMod;
+        if(mod(numWrap, 2.) == 0.){
+            return high - difMod;
+        } else {
+            return low + difMod;
+        }
+    }
+    if(val < low){
+        float dif = low-val;
+        float difMod = mod(dif, range);
+        float numWrap = dif/range - difMod;
+        if(mod(numWrap, 2.) == 0.){
+            return low + difMod;
+        } else {
+            return high - difMod;
+        }
+    }
+    return val;
+}
+vec2 wrap(vec2 val, float low, float high){
+    return vec2(wrap3(val.x, low, high), wrap3(val.y, low, high));
+}
+
+//slice the matrix up into columns and translate the individual columns in a moving wave
+vec2 columnWaves3(vec2 stN, float numColumns, float time2, float power){
+    return vec2(wrap3(stN.x + sin(time2*8.)*0.05 * power, 0., 1.), wrap3(stN.y + cos(quant(stN.x, numColumns)*5.+time2*2.)*0.22 * power, 0., 1.));
+}
+
+//slice the matrix up into rows and translate the individual rows in a moving wave
+vec2 rowWaves3(vec2 stN, float numColumns, float time2, float power){
+    return vec2(wrap3(stN.x + sin(quant(stN.y, numColumns)*5.+time2*2.)*0.22 * power, 0., 1.), wrap3(stN.y + cos(time2*8.)*0.05 * power, 0., 1.));
+}
+
+
+//iteratively apply the rowWave and columnWave functions repeatedly to 
+//granularly warp the grid
+vec2 rowColWave(vec2 stN, float div, float time2, float power){
+    for (int i = 0; i < 10; i++) {
+        stN = rowWaves3(stN, div, time2, power);
+        stN = columnWaves3(stN, div, time2, power);
+    }
+    return stN;
+}
+
+vec4 colormap_hsv2rgb(float h, float s, float v) {
+    float r = v;
+    float g = v;
+    float b = v;
+    if (s > 0.0) {
+        h *= 6.0;
+        int i = int(h);
+        float f = h - float(i);
+        if (i == 1) {
+            r *= 1.0 - s * f;
+            b *= 1.0 - s;
+        } else if (i == 2) {
+            r *= 1.0 - s;
+            b *= 1.0 - s * (1.0 - f);
+        } else if (i == 3) {
+            r *= 1.0 - s;
+            g *= 1.0 - s * f;
+        } else if (i == 4) {
+            r *= 1.0 - s * (1.0 - f);
+            g *= 1.0 - s;
+        } else if (i == 5) {
+            g *= 1.0 - s;
+            b *= 1.0 - s * f;
+        } else {
+            g *= 1.0 - s * (1.0 - f);
+            b *= 1.0 - s;
+        }
+    }
+    return vec4(r, g, b, 1.0);
+}
+
+vec4 colormap(float x) {
+    float h = clamp(-7.44981265666511E-01 * x + 7.47965390904122E-01, 0.0, 1.0);
+    float s = 1.0;
+    float v = 1.0;
+    return colormap_hsv2rgb(h, s, v);
+}
+
+float colourDistance(vec3 e1, vec3 e2) {
+  float rmean = (e1.r + e2.r ) / 2.;
+  float r = e1.r - e2.r;
+  float g = e1.g - e2.g;
+  float b = e1.b - e2.b;
+  return sqrt((((512.+rmean)*r*r)/256.) + 4.*g*g + (((767.-rmean)*b*b)/256.));
+}
+
+vec4 circleSlice(vec2 stN, float t, float randw){
+    
+    //define several different timescales for the transformations
+    float t0, t1, t2, t3, t4, rw;
+    t0 = t/4.5;
+    t1 = t/2.1;
+    t2 = t/1.1;
+    t3 = t/0.93;
+    rw =  randw/290.; //a random walk value used to parameterize the rotation of the final frame
+    t4 = t;
+    
+    t1 = t1 / 2.;
+    t0 = t0 / 2.;
+    rw = rw / 2.;
+    float divx = sinN(t0) * 120.+10.;
+    float divy = cosN(t1) * 1400.+10.;
+    stN = stN * rotate(stN, vec2(0.5), rw);
+    vec2 trans2 = vec2(mod(floor(stN.y * divx), 2.) == 0. ? mod(stN.x + (t1 + rw)/4., 1.) : mod(stN.x - t1/4., 1.), 
+                       mod(floor(stN.x * divy), 2.) == 0. ? mod(stN.y + t1, 1.) : mod(stN.y - t1, 1.));
+    
+    
+    bool inStripe = false;
+    float dist = distance(trans2, vec2(0.5));
+
+
+    float numStripes = 20.;
+    float d = 0.05;
+    float stripeWidth =(0.5 - d) / numStripes;
+    for(int i = 0; i < 100; i++){
+        if(d < dist && dist < d + stripeWidth/2.) {
+            inStripe = inStripe || true;
+        } else {
+            inStripe = inStripe || false;
+        }
+        d = d + stripeWidth;
+        if(d > 0.5) break;
+    }
+    
+    vec4 c = !inStripe ? vec4(white, 1) : vec4(black, 0);
+    return c;
+    
+}
+
+
+
 vec3 coordWarp(vec2 stN, float t2){ 
     vec2 warp = stN;
     
@@ -29,71 +174,66 @@ vec3 coordWarp(vec2 stN, float t2){
     return vec3(warp, distance(warp, stN));
 }
 
-
-float rand2(float f, float d) {vec2 n = vec2(f, rand(f)); return (fract(1e4 * sin(17.0 * n.x + n.y * 0.1) * (0.1 + abs(sin(n.y * 13.0 + n.x))))-0.5)*d;}
-
-vec4 avgColorBB(vec2 nn, float dist, float d){
-    vec4 c1 = texture2D(backbuffer, nn+vec2(0, dist)      +rand2(1., d)).rgba;
-    vec4 c2 = texture2D(backbuffer, nn+vec2(0, -dist)     +rand2(2., d)).rgba;
-    vec4 c3 = texture2D(backbuffer, nn+vec2(dist, 0)      +rand2(3., d)).rgba;
-    vec4 c4 = texture2D(backbuffer, nn+vec2(-dist, 0)     +rand2(4., d)).rgba;
-    vec4 c5 = texture2D(backbuffer, nn+vec2(dist, dist)   +rand2(5., d)).rgba;
-    vec4 c6 = texture2D(backbuffer, nn+vec2(-dist, -dist) +rand2(6., d)).rgba;
-    vec4 c7 = texture2D(backbuffer, nn+vec2(dist, -dist)  +rand2(7., d)).rgba;
-    vec4 c8 = texture2D(backbuffer, nn+vec2(-dist, dist)  +rand2(8., d)).rgba;
+vec2 multiBallCondition(vec2 stN, float t2){
     
-    return (c1+c2+c3+c4+c5+c6+c7+c8)/8.;
-}
-
-void ex1(){
-    vec2 stN = uvN(); //function for getting the [0, 1] scaled corrdinate of each pixel
+    float rad = .08;
+    bool cond = false;
+    float ballInd = -1.;
     
-    float t2 = time/2.; //time is the uniform for global time
-    stN = quant(stN, pow((1.-sliderVals[5]), 4.)*2000.+4.);
-    vec3 warpN = coordWarp(stN, time);
-    vec3 col;
-    float swing = +sinN(time*PI*4.)*0.3; //make this bass?
-    for(float i = 0.; i < 6.; i++){
-        float iSwing = i*pow(sinN(time/4.), 0.3)*(1.+sliderVals[3]*2.);
-        vec2 start = rotate(vec2(0.3)-sliderVals[3], vec2(0.5), iSwing);
-        vec2 center = mix(start, vec2(0.7), sinN(time+iSwing+swing));
-        vec2 warpCent = coordWarp(center, time/10.).xy;
-        col =(distance(stN, warpCent) < 0.03 + sliderVals[3]*0.1 ? black : white) + col;
-    }
-    col /= 6.;
-    // col.xy = warpN.xy;
-
-    //like laserGlow_slider, make some kind of coordinate noise grain effect the highs?
-    
-    vec3 hashN = hash(vec3(stN, time))-0.5;
-    
-    float feedback; 
-    vec4 bbN = texture2D(backbuffer, stN+hashN.xy*0.1);
-    vec4 bbNoise = texture2D(backbuffer, stN);
-    vec4 bbWarp = texture2D(backbuffer, rotate(stN, vec2(0.5), PI/8.*(1.-sliderVals[4]))+hashN.xy*0.1*sliderVals[0]);
-    vec2 trailPoint = vec2(0.5); //mix(vec2(0.5), coordWarp(vec2(0.5), time).xy, 2.5);
-    vec2 warpMix = mix(mix(stN, warpN.xy, 0.01), trailPoint, 0.01 * sin(time/3.));
-    vec4 bb = avgColorBB(warpMix, 0.005, 0.01);
-    
-    bool condition = col == white;
-    
-    if(condition){
-        feedback = bb.a * 0.99;
-    } 
-    else{
-        feedback = 1.;
+    for (int i = 0; i < 20; i++) {
+        float i_f = float(i);
+        vec2 p = vec2(sinN(t2 * rand(vec2(i_f+1., 10.)) * 1.3 + i_f), cosN(t2 * rand(vec2(i_f+1., 10.)) * 1.1 + i_f));
+        cond = cond || distance(stN, p) < rad;
+        if(distance(stN, p) < rad) ballInd = float(i); 
     }
     
-    vec3 cc = (feedback > 0.5 ? white : black) - feedback;
-    cc = vec3(sinN(feedback*(5.+sliderVals[1]*45.)));
-    
-    cc = mix(cc, bbWarp.rgb, 0.5);
-    
-    //the fragment color variable name (slightly different from shader toy)
-    float c = cc.x > 0.5 ? pow(cc.x, 1.-sliderVals[2]) : pow(cc.x, 1.+sliderVals[2]);
-    gl_FragColor = time < 1. ? vec4(0.) : vec4(vec3(c), feedback);
+    return vec2(cond ? 1. :0., ballInd/20.);
 }
 
-void main(){
-    ex1();
+// calculates the luminance value of a pixel
+// formula found here - https://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color 
+vec3 lum(vec3 color){
+    vec3 weights = vec3(0.212, 0.7152, 0.0722);
+    return vec3(dot(color, weights));
+}
+
+float sigmoid(float x){
+    return 1. / (1. + exp(-x));
+}
+
+float rampAD(float inputTime, float peakTime){
+    if(inputTime == peakTime) return 1.;
+    if(inputTime < peakTime) return inputTime/peakTime;
+    if(inputTime > peakTime) return 1.-(inputTime-peakTime)/(1.-peakTime);
+}
+
+void main () {
+    vec2 stN = uvN();
+    vec2 cent = vec2(0.5);
+
+    vec3 p5 = texture2D(channel1, stN).rgb;
+    //slider to control some region split parameters, and maybe band-num per split
+    //slider to control which bands are on or off? can have some thing so that you cycle between a few
+    float t2 = 1000. + sigmoid(sin(time/5.)*5.)*2. + rand(quant(stN.x, 500.*pow(sliderVals[2] ,5.)+1.))*10.; 
+    float t3 = 00000.;
+    
+    vec3 hashN = hash(vec3(stN, time));
+
+    stN = stN+(hashN.xy - 0.5)*pow(sinN(time*PI*8.), 2.)*sliderVals[4]*0.2;//bands.y
+    //add some coordinate hazing/blurring noise? high frequencies mapped to hash() displacement of stN
+    stN.x = mix(stN, coordWarp(quant(stN, pow(sliderVals[5], 5.)*1000.+4.), time).xy, sliderVals[0]*2.).y;
+    float s1 = pow(sliderVals[1], 5.)*50.+0.05;
+    vec3 col = vec3(pow(sinN(time + stN.x*PI*3.), 50.)*4., //slider for warp line sharpness
+                    sliderVals[6]*pow(cosN(5.*t2/ (10. + sinN(stN.y + time/16.*PI+pow(sinN(time*PI*4.), 2.)*0.0))), s1), //slider for horizontal line width +bounce down at bands.x
+                    sliderVals[6]*sinN(time/5.));  
+                    
+                    
+    // col = col == vec3(10./255.) ? vec3(sinN(time + stN.x*PI  + rampAD(sliderVals[0], 0.3)/2.), cosN(50.*t3/ (10. + sinN(stN.y + time/16.*PI))), sinN(time/5.)) : col;
+    vec4 bb = texture2D(backbuffer, uvN());
+    float fdbk = 0.8 + sigmoid(sin(time+stN.x*PI)*10.)*0.2;
+    float fdbk2 = 0.3 + sigmoid(sin(time+stN.x*PI)*10.)*0.7;
+    // col = mix(col, bb.rgb, pow(fdbk, 150.));
+    // col = mix(black, col, sigmoid((pow(fdbk, 3.)-0.8)*100. ));
+    // if(pow(fdbk, 150.) > 0.95) col = mix(col, black, 0.02);
+    gl_FragColor = vec4(col, 1.);
 }
